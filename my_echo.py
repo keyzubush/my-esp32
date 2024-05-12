12ffc57d299dc662f81d948157f4bcee40e457a2
Unknown <ikosachiov@gmial.com>
2024-05-12 15:28:04 +0300

    stone

--- my_echo.py
+++ my_echo.py
@@ -3,12 +3,13 @@ from hcsr04 import HCSR04
 from time import sleep
 
 # ESP32
-sensor = HCSR04(trigger_pin=15, echo_pin=18, echo_timeout_us=10000)
-
+echo_right = HCSR04(trigger_pin=15, echo_pin=19, echo_timeout_us=10000)
+echo_front = HCSR04(trigger_pin=2, echo_pin=5, echo_timeout_us=10000)
 # ESP8266
 #sensor = HCSR04(trigger_pin=12, echo_pin=14, echo_timeout_us=10000)
 
 while True:
-    distance = sensor.distance_cm()
-    print('Distance:', distance, 'cm')
+    distance_right = echo_right.distance_cm()
+    distance_front = echo_front.distance_cm()
+    print(distance_front, distance_right)
     sleep(0.1)
