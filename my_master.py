8e2ce0bc4ecaa7ed57612e2ea2a852f81e611509
Serge Kosachiov <skosachiov@gmail.com>
2025-04-08 17:58:19 +0300

    slave

--- my_master.py
+++ my_master.py
@@ -1,33 +1,88 @@
-import board, asyncio, busio, json
+import board, asyncio, busio, json, espcamera
+from ulab import numpy as np
+
+i2c = board.I2C()
+i2c.unlock()
+
+cam = espcamera.Camera(
+    data_pins=board.CAMERA_DATA,
+    external_clock_pin=board.CAMERA_XCLK,
+    pixel_clock_pin=board.CAMERA_PCLK,
+    vsync_pin=board.CAMERA_VSYNC,
+    href_pin=board.CAMERA_HREF,
+    powerdown_pin=board.CAMERA_PWDN,
+    reset_pin=None,
+    i2c=i2c,
+    external_clock_frequency=20_000_000,
+    pixel_format=espcamera.PixelFormat.GRAYSCALE,
+    frame_size=espcamera.FrameSize.R96X96,
+    framebuffer_count=2,
+    grab_mode=espcamera.GrabMode.WHEN_EMPTY)
+cam.saturation = 2
+cam_shape = (96, 96)
 
 TIMEOUT = 0.05
+STRIPE = 8
 # uart = busio.UART(board.TX2, board.RX2, baudrate=115200, timeout = TIMEOUT)
 uart = busio.UART(board.IO12, board.IO13, baudrate=115200, timeout = TIMEOUT)
 
-msg_rx = {'id': 0, 'timestamp': 0, 'left': 0, 'right': 0, 'duration': -1, 'hash': 0}
-msg_tx = {'id': 0, 'timestamp': 0, 'distance': 999, 'speed': 0, 'angle': 0, 'hash': 0}
+msg_tx = {'id': 0, 'timestamp': 0, 'left': 0, 'right': 0, 'duration': -1, 'hash': 0}
+msg_rx = {'id': 0, 'timestamp': 0, 'distance': 999, 'speed': 0, 'angle': 0, 'hash': 0}
+
+def msg_debug(msg, checks):
+    msg_prev = msg
+    def msg_debug_inner(msg):
+        nonlocal msg_prev
+        for c in checks:
+            if msg_prev[c] != msg[c]:
+                print(msg)
+                msg_prev = msg.copy()
+                break
+    return msg_debug_inner
+
+msg_debug_tx = msg_debug(msg_rx, ['left', 'right'])
+msg_debug_rx = msg_debug(msg_tx, ['distance', 'speed', 'angle'])
+
+async def camera():
+    global msg_rx, msg_tx
+    while True:
+        buf_orig = cam.take(1)
+        buf = bytearray(buf_orig)
+        gray = np.frombuffer(buf, dtype=np.uint8)
+        gray.shape = cam_shape
+        min_col = list(np.argmin(gray, axis = 1))
+        s = []
+        NORM = STRIPE * cam_shape[1] / 2
+        for i in range(cam_shape[0], cam_shape[0]//2, STRIPE):
+            s.append(sum(gray[i-STRIPE: i]) - (cam_shape[0]//2) * STRIPE)
+        msg_tx['left'] = 0.75 - s[0]/NORM
+        msg_tx['left'] = 0.75 + s[0]/NORM
+        await asyncio.sleep(TIMEOUT)   
 
 async def uart_write(uart):
     global msg_rx, msg_tx
     while True:
-        interval = random.uniform(0.25, 2.5)
-        uart.write(json.dumps(msg).encode('utf-8'))
-        print('WRITE:', msg)
-        msg['id'] += 1
-        msg['hash'] = hash(msg['id'])
-        await asyncio.sleep(interval)   
+        msg_tx['id'] += 1
+        msg_tx['timestamp'] = time.time()
+        msg_debug_tx(msg_tx)
+        uart.write(json.dumps(msg_tx).encode('utf-8'))
+        await asyncio.sleep(TIMEOUT)   
 
 async def uart_read(uart):
     global msg_rx, msg_tx
     while True:
         ba = uart.read()
         if ba:
-            print('READ:', json.loads(ba.decode('uft-8')))
+            msg_rx = json.loads(ba.decode('uft-8'))
+            msg_debug_rx(msg_rx)
         await asyncio.sleep(TIMEOUT)   
 
 async def main():
-    uw_task = asyncio.create_task(uart_write(uart))
-    ur_task = asyncio.create_task(uart_read(uart))
-    await asyncio.gather(uw_task, ur_task)
+    await asyncio.gather(
+        asyncio.create_task(camera()),
+        asyncio.create_task(uart_write(uart)),
+        asyncio.create_task(uart_read(uart)))
 
 asyncio.run(main())
+cam.deinit()
+
